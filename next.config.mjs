0d39cd3834fa6edd879fa1736dcd87796ebf179a
moritzcodes <moritz.j.kindler@gmail.com>
2025-03-13 14:09:12 +0100

    File size

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  api: {
+    bodyParser: {
+      sizeLimit: '100mb',
+    },
+  },
+};
 
 export default nextConfig;
